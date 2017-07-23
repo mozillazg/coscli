e56fcacc5bc8f7c686bbb1bcf4ed2f0f8a07f534
mozillazg <mozillazg101@gmail.com>
2017-07-23 22:34:52 +0800

    增加查看 bucket 列表的 bucketList/bl 命令

---
+++ cmd/bucket_list.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/mozillazg/go-cos"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+var bucketListCmd = &cobra.Command{
+	Use:     "bucketList",
+	Aliases: []string{"bl"},
+	Short:   "List buckets",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := listBuckets()
+		if err != nil {
+			exitWithError(err)
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(bucketListCmd)
+}
+
+func listBuckets() error {
+	client := cos.NewClient(nil,
+		&http.Client{
+			Transport: authTransport,
+			Timeout:   time.Second * time.Duration(globalConfig.timeout),
+		},
+	)
+	ctx := context.Background()
+	ret, _, err := client.Service.Get(ctx)
+	if err != nil {
+		return err
+	}
+	formatBucketList(ret.Buckets)
+	return nil
+}
+
+func formatBucketList(buckets []cos.Bucket) {
+	table := tablewriter.NewWriter(os.Stdout)
+	number := len(buckets)
+	data := make([][]string, number, number)
+	for n, b := range buckets {
+		data[n] = []string{
+			b.Name, b.Region, b.CreateDate,
+		}
+	}
+	table.SetHeader([]string{"Name", "Region", "CreateDate"})
+	table.AppendBulk(data)
+	table.Render()
+	return
+}
